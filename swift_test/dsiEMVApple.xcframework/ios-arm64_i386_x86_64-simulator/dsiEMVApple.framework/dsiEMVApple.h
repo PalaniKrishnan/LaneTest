8650ba85307f2f6edf33e141ee5bb47506dc8e66
Palani Krishnan <palanikrishnan8@gmail.com>
2024-06-13 15:06:52 +0530

    initial commit

---
+++ swift_test/dsiEMVApple.xcframework/ios-arm64_i386_x86_64-simulator/dsiEMVApple.framework/dsiEMVApple.h
@@ -0,0 +1,37 @@
+#import <Foundation/Foundation.h>
+
+// Controls the naming convention of the framework APIs (PascalCase vs. CamelCase or use both).
+//
+// This was needed for Swift as it doesn't allow PascalCase, only CamelCase.
+// The __BRIDGING__ #define exists to remove the PascalCase for the framework APIs otherwise an "ambiguous use" will occur.
+// When using the framework from an Objective-C project, comment out the following #define.
+#define __BRIDGING__
+
+@protocol dsiEMVAppleDelegate < NSObject >
+@optional
+    - ( void ) connectionResponse : ( NSString* ) response;     // Called from EstablishBluetoothConnection.
+    - ( void ) displayMessage : ( NSString* ) message;          // Called when a message is generated from Datacap library calls.
+    - ( void ) transactionResponse : ( NSString* ) response;    // Called from ProcessTransaction or GetDevicesInfo.
+@end
+
+@interface dsiEMVApple : NSObject
+	#ifndef __BRIDGING__
+		// PascalCase for 'C', C++, and Objective-C.
+		- ( NSString* ) ProcessTransaction : ( NSString* ) nsRequest;
+		- ( NSString* ) GetDevicesInfo;
+		- ( void ) CancelTransaction;
+		- ( void ) Disconnect;
+	    - ( NSString* ) EstablishBluetoothConnection : ( NSString* ) nsBluetoothDeviceName;
+	    - ( bool ) IsBluetoothConnected;
+	#endif
+
+	// CamelCase for Swift and Objective-C
+	- ( NSString* ) processTransaction : ( NSString* ) nsRequest;
+	- ( NSString* ) getDevicesInfo;
+	- ( void ) cancelTransaction;
+	- ( void ) disconnect;
+    - ( NSString* ) establishBluetoothConnection : ( NSString* ) nsBluetoothDeviceName;
+    - ( bool ) isBluetoothConnected;
+
+    @property( nullable, nonatomic, weak ) id < dsiEMVAppleDelegate > delegate;
+@end  
